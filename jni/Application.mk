c96488422bd61971b85e50aedb661cc466f072e9
Kevin Yang <skyang@gmail.com>
2016-02-23 13:32:58 -0500

    Add android makefiles for jsoncpp
    	new file:   jni/Android.mk
    	new file:   jni/Application.mk
    	new file:   jni/README.md

---
+++ jni/Application.mk
@@ -0,0 +1,8 @@
+APP_ABI := armeabi armeabi-v7a x86
+APP_STL := gnustl_shared
+NDK_TOOLCHAIN_VERSION := 4.8
+
+APP_CPPFLAGS += -frtti
+APP_CPPFLAGS += -fexceptions
+
+APP_MODULES := jsoncpp
---
+++ jni/README.md
@@ -0,0 +1,8 @@
+We will build two jsoncpp into a static android library.
+
+To build:
+
+cd jsoncpp/lib
+ndk-build
+
+Requires NDK
